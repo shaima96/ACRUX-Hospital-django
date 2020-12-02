562825ab472f5a37ee18657faea7e861aeaa7a13
Moumenh <mohanbali94@gmail.com>
2020-12-02 23:12:15 +0200

    adding role attr

--- Backend/users/models.py
+++ Backend/users/models.py
@@ -26,13 +26,14 @@ class UserAccountManager(BaseUserManager):
 class UserAccount(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
+    role = models.CharField(max_length=255,default="user")
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserAccountManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name']
+    REQUIRED_FIELDS = ['name','role']
 
     def get_full_name(self):
         return self.name
