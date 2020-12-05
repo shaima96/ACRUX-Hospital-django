dc7bf554085a6abc6314f9a6d8e0fcc438339e63
Moumenh <mohanbali94@gmail.com>
2020-12-05 11:36:36 +0200

    patient-doctors manytomany table

--- Backend/patient/admin.py
+++ Backend/patient/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from patient.models import Patient
+from patient.models import DoctorPatient
 # Register your models here.
+admin.site.register(Patient)
+admin.site.register(DoctorPatient)
--- Backend/patient/models.py
+++ Backend/patient/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from users.models import UserAccount
+from doctor.models import Doctor
 # Create your models here.
+
+class Patient(models.Model):
+    userId = models.OneToOneField(UserAccount,on_delete=models.CASCADE)
+    role = models.CharField(max_length=10,default='patient')
+    BloodType = models.CharField(max_length=10)
+    image = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.userId.name
+
+
+class DoctorPatient(models.Model):
+    patientId = models.ForeignKey(Patient,on_delete=models.CASCADE)
+    doctorId = models.ForeignKey(Doctor,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "%s %s" % (self.doctorId.doctor.name, self.patientId.userId.name)
