e99cb0264eea3ac65fa603e6fb388b98d4e46493
asemnouri <asem_nori@hotmail.com>
2020-12-06 14:25:13 +0200

    try

--- client/src/Components/SharedComponents/Header/SignIn.jsx
+++ client/src/Components/SharedComponents/Header/SignIn.jsx
@@ -40,13 +40,14 @@ class SignIn extends React.Component {
             .then(data => {
                 console.log("aaaaaaaaaaa",data)
                 localStorage.setItem("Authorization",`JWT ${data.access}`)
-                let x=this.loadUser ||this.props.loadUser
+                let loader=this.loadUser ||this.props.loadUser
                 // this.loadUser?
                 // this.loadUser()
                 // :this.props.loadUser()
-                return x()
+                this.setState({email: '',password: ''})
+                return loader()
             })
-            .then(async (data)=>{
+            .then((data)=>{
                 console.log("DISPATCH ME",data)
                  this.props.setCurrentUser(data)
             })
