268e6cf513f1bf36be3ec7d17600355fb86478bc
israakittaneh <israakittanehm@gmail.com>
2020-12-07 15:47:01 +0200

    fetch data department & render it

--- client/src/Components/SharedComponents/Header/Header.jsx
+++ client/src/Components/SharedComponents/Header/Header.jsx
@@ -24,6 +24,7 @@ const Header = ({ currentUser, setCurrentUser }) => {
                                 id: null
                             })
                         }}> LogOut </Button>
+                        
                         </div>
                         :
                         <RegisterDialog />
