4a07affaaf76868dc879e397e28343fb176b935d
asemnouri <asem_nori@hotmail.com>
2020-12-08 02:05:13 +0200

    adding some additional components

--- client/src/Components/Pages/UserProfilePage/DoctorProfile.jsx
+++ client/src/Components/Pages/UserProfilePage/DoctorProfile.jsx
@@ -1,33 +1,16 @@
 import React from "react"
-
+import "./UserProfile.css"
 
 const DoctorProfile = () => {
     return (
-        <div className="profile">
-            <div className="profile__left">
-                circular image 30%
-            </div>
-            <div className="profile__right">
-                <div className="right__image">
-                    circular image 70%
-            </div>
-                <div className="right__content">
-                    <div className="content__left">
-                        <div className="content__left__collection">
-                            popular collection
-                        </div>
-                        <div className="content__left__royal">
-                            royal ui force
-                        </div>
-
-                    </div>
-                    <div className="content__right">
-                        details
-                    </div>
-                </div>
-
-            </div>
+        <div className="content__left">
+        <div className="content__left__collection">
+            popular collection
         </div>
+        <div className="content__left__royal">
+            royal ui force
+        </div>
+    </div>
     )
 
 
