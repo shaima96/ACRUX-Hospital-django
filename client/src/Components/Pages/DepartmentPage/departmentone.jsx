c996404af47fe78207f52643e49316a05d715989
asemnouri <asem_nori@hotmail.com>
2020-12-20 21:22:18 +0200

    department page

--- client/src/Components/Pages/DepartmentPage/departmentone.jsx
+++ client/src/Components/Pages/DepartmentPage/departmentone.jsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import { Card, CardActionArea, CardActions, CardContent, CardMedia, Button, Typography, makeStyles } from '@material-ui/core';
 import { Link } from 'react-router-dom'
-
+import "./depCard.css"
 const useStyles = makeStyles({
   root: {
     width: 380,
@@ -15,18 +15,16 @@ const useStyles = makeStyles({
 
 
 
-export default function DepCard({department}) {
+export default function DepOne({department,image}) {
   const classes = useStyles();
   return (
     <Link to={`/department/${department.pk}`}  style={{textDecoration:'none'}}>
-    <Card className={classes.root} style={{margin:'30px'}}>
-            <CardActionArea>
-                <CardMedia
-                    className={classes.media}
-                    image={department.image}
-                    title="Contemplative Reptile"
-                />
-                <CardContent>
+    <Card className={classes.root} className="cardit" style={{marginBottom:'17px',marginRight:"0px",marginTop:"17px",width:'100%'}}>
+            <CardActionArea style={{display:"flex",width:"550px"}} className="try">
+                <div className='zameel' style={{overflow:"hidden",height:"100px",width:"100px",display:"flex",alignItems:"center",justifyContent:"center",borderRadius:"100PX",boxShadow:"3px 4px 15px #00000026", marginLeft:'15px'}} >
+                    <img src={image} style={{width:"60px",height:"60px"}} alt="image"/>
+                </div>
+                <CardContent style={{textAlign:"start",width:"450px"}}>
                     <Typography gutterBottom variant="h5" component="h2">
                         {department.title}
             </Typography>
