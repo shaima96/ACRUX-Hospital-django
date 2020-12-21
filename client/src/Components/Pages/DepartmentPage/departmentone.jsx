be965af2ebf77836814508ee725b33a61a50f739
Moumenh <mohanbali94@gmail.com>
2020-12-21 10:10:33 +0200

    transions dep card

--- client/src/Components/Pages/DepartmentPage/departmentone.jsx
+++ client/src/Components/Pages/DepartmentPage/departmentone.jsx
@@ -22,7 +22,7 @@ export default function DepOne({department,image}) {
     <Card className={classes.root} className="cardit" style={{marginBottom:'17px',marginRight:"0px",marginTop:"17px",width:'100%'}}>
             <CardActionArea style={{display:"flex",width:"550px"}} className="try">
                 <div className='zameel' style={{overflow:"hidden",height:"100px",width:"100px",display:"flex",alignItems:"center",justifyContent:"center",borderRadius:"100PX",boxShadow:"3px 4px 15px #00000026", marginLeft:'15px'}} >
-                    <img src={image} style={{width:"60px",height:"60px"}} alt="image"/>
+                    <img src={image} alt="image"/>
                 </div>
                 <CardContent style={{textAlign:"start",width:"450px"}}>
                     <Typography gutterBottom variant="h5" component="h2">
